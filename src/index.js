87a51b22d5cb63505f07333409e280433e5cf897
Flavia Oliveira <flaviacoliv@hotmail.com>
2023-04-09 18:41:20 -0300

    Atualização 1.0.1

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// window.alert("Olá Mundo!")
+// window.confirm("Quer continuar?")
+// window.prompt("Digite sua idade.")
+
+// var nomePessoa = "Flávia";
+// console.log(nomePessoa)
+
+// const emailPessoa = "flaviacoliv@hotmail.com"
+// console.log(emailPessoa)
+
+// let telefonePessoa = "71983628135"
+// console.log(telefonePessoa)
+
+// let idade = prompt("Digite sua idade")
+
+// console.log(idade)
+let nome = "Flávia";
+
+
+
+
+
+
