47fd5dcb6c2c7abcfc68bbfcf08caa9aa7f74e6d
Flavia Oliveira <flaviacoliv@gmail.com>
2024-09-13 22:22:41 -0300

    JS BASICO 130924 3

--- src/index.js
+++ src/index.js
@@ -14,17 +14,24 @@
 // let idade = prompt("Digite sua idade")
 
 // console.log(idade)
-let nome = "Flávia";
-let numero = 0.6666666666
-let usuarioVerificado = true
-if(usuarioVerificado){
-  console.log("Usuário Verificado.")
-}else{
-  console.log("Usuário Não é Verificado.")
-}
-
-let pessoa = null;
+/////////////////////////////////////////////
+// let nome = "Flávia";
+// let numero = 0.6666666666
+// let usuarioVerificado = true
+// if(usuarioVerificado){
+//   console.log("Usuário Verificado.")
+// }else{
+//   console.log("Usuário Não é Verificado.")
+// }
+////////////////////////////////////////////
+// let pessoa = null;
 
 // console.log("A cada tarefa feita, sabe-se mais sobre a pureza da alma. Porém surge a dúvida: ''E quando a alma não é mais pura? Se essa pureza não foi retirado por escolha, mas por imposição em uma época que a capacidade de decição era bem remota e fraca, por haver pouco desenvolvimento da alma e da própria vidaa, por ter pouca idade e pouquíssimo tempo de desenvolvimento?''");
 
 // console.log("A Resposta está na escolha, de quando se tomou consciência sobre a possibilidade daquele ato e escolha ser um fator de impureza da alma, parar imediatamente, se perdoar, e nunca mais repetir aquela atitute, aquele ato. Escolher fazer diferente do que foi inflenciado até então, e fazer a NOVA vida de acordo ao que traz de volta pureza da alma, que nunca foi perdida, só sofreu sujicidade e não sua extinção. Com as escolhas certas, recupera-se aquela pureza que estava escondida.") 
+/////////////////////////////////////////////////
+const a = 3
+const b = 3
+
+console.log(a==b)
+console.log(a!=b)
