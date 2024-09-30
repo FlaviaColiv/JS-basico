ed6347f8525ba578059284ed85b7f7ce24a0cca1
Flavia Oliveira <flaviacoliv@hotmail.com>
2024-09-30 03:29:38 -0300

    js 300924 3

--- src/atividade.js
+++ src/atividade.js
@@ -1,7 +1,15 @@
 // Solicitar nome do usuário
 
-var nome = prompt("Por favor, digite seu nome:");
+const nome = prompt("Por favor, digite seu nome:");
 
 // Apresenta mensagem de boas-vindas
 alert("Seja Bem-vindo(a), " + nome + "!");
+
+const boletoPago = false;
+
+if (boletoPago) {
+    alert("O boleto está pago, " + nome + ".")
+} else {
+    alert("O boleto não está pago, " + nome + ".")
+}
 
